3edbd1b9eccbf68fda3be142b3a9b17c78b58a09
Igor <igormatevosian@gmail.com>
2021-12-18 15:49:25 +0300

    Update main.c

--- Archiver/main.c
+++ Archiver/main.c
@@ -5,18 +5,20 @@
 
 int create(FILE *fileArc, FILE *fileAdd, const char *fileName) {
     if (fileAdd == NULL)return 1;
-    char symbol = fgetc(fileAdd);
     int fileSize = 0;
     while (!feof(fileAdd)) {
         ++fileSize;
         fgetc(fileAdd);
     }
-    fwrite(fileName, 12, 1, fileArc);
+    char sizeOfName[sizeof(int)];
+    sprintf(sizeOfName, "%d", sizeof(fileName));
+    fwrite(sizeOfName, sizeof(int), 1, fileArc);
+    fwrite(fileName, sizeof(fileName)*2, 1, fileArc);
     fwrite(&fileSize, sizeof(int), 1, fileArc);
-    fseek(fileAdd, 1, SEEK_SET);
+    fseek(fileAdd, 0, SEEK_SET);
     for (int i = 0; i < fileSize; i++) {
+        char symbol = fgetc(fileAdd);
         fputc(symbol, fileArc);
-        symbol = fgetc(fileAdd);
     }
     fclose(fileAdd);
     return 0;
@@ -27,12 +29,13 @@ int extract(FILE *fileArc, const char *arcName) {
     if (fileArc == NULL){
         return 1;
     }
-    printf("From %s:\n", arcName);
     while (!feof(fileArc)) {
         char fileName[128] = {0};
         int fileSize;
         FILE *outputFile;
-        fread(fileName, 12, 1, fileArc);
+        char fileNameSize[sizeof(int)];
+        fread(fileNameSize, sizeof(int), 1, fileArc);
+        fread(fileName, atoi(fileNameSize)*2, 1, fileArc);
         if (strstr(fileName, ".") == NULL) {
             return 0;
         }
@@ -41,6 +44,7 @@ int extract(FILE *fileArc, const char *arcName) {
         for (int i = 0; i < fileSize; i++) {
             fputc(fgetc(fileArc), outputFile);
         }
+        printf("From %s:\n", arcName);
         printf("%s was extracted\n", fileName);
     }
     return 0;
@@ -52,7 +56,9 @@ void list(FILE *fileArc, const char *arcName){
     while (!feof(fileArc)) {
         char fileName[128] = {0};
         int fileSize;
-        fread(fileName, 12, 1, fileArc);
+        char fileNameSize[sizeof(int)];
+        fread(fileNameSize, sizeof(int), 1, fileArc);
+        fread(fileName, atoi(fileNameSize)*2, 1, fileArc);
         if (strstr(fileName, ".") == NULL) {
             return;
         }
@@ -77,16 +83,12 @@ int main(int argc, char const *argv[]) {
         if (!strcmp(argv[i], "--create")) {
             arcFile = fopen(arcName, "wb");
             for (int j = i + 1; j < argc; j++) {
-                if (strstr(argv[j], ".") == NULL) {
-                    break;
-                } else {
-                    FILE *file = fopen(argv[j], "rb");
-                    if (create(arcFile, file, argv[j])) {
-                        printf("Can't open file: %s\n", argv[j]);
-                        return 1;
-                    } else{
-                        printf("File %s added to %s\n", argv[j], arcName);
-                    }
+                FILE *file = fopen(argv[j], "rb");
+                if (create(arcFile, file, argv[j])) {
+                    printf("Can't open file: %s\n", argv[j]);
+                    return 1;
+                } else{
+                    printf("File %s added to %s\n", argv[j], arcName);
                 }
             }
         }
